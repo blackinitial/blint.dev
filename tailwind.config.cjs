6f5f042c254c574db04da43d68ab053bd55a9713
blackinitial <alhanif.blackinitial@gmail.com>
2023-02-03 22:43:22 +0700

    add hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			borderRadius: {
+        '4xl': '2rem',
+				'5xl': '3rem',
+				'6xl': '5rem',
+      }
+		},
 	},
 	plugins: [],
 };
