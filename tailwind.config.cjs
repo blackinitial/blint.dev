70d433eb202865dd80898540181d60e9dfe62e81
blackinitial <alhanif.blackinitial@gmail.com>
2023-02-05 16:08:46 +0700

    add dark mode, toggle theme, layout, css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'media',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
@@ -6,8 +7,21 @@ module.exports = {
         '4xl': '2rem',
 				'5xl': '3rem',
 				'6xl': '5rem',
-      }
-		},
+      },
+			colors: {
+				'brand-primary': 'var(--brand-primary)',
+				'brand-secondary': 'var(--brand-secondary)',
+				'brand-tertiary': 'var(--brand-tertiary)'
+			},
+			backgroundColor: {
+        brand: 'var(--brand-bg)',
+				'brand-card': 'var(--brand-card)'
+      },
+      textColor: {
+        brand: 'var(--brand-text)',
+				'brand-head': 'var(--brand-head)'
+      },
+		}
 	},
 	plugins: [],
 };
