3a9895d7c0109a668b3294d7e90ddb7b8ca02595
blackinitial <alhanif.blackinitial@gmail.com>
2023-06-15 08:01:35 +0700

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import image from "@astrojs/image";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image(), react()],
   vite: {
     ssr: {
-      noExternal: ["@radix-ui/*"],
-    },
+      noExternal: ["@radix-ui/*"]
+    }
   },
+  output: "server",
+  adapter: vercel({
+    analytics: true
+  })
 });
