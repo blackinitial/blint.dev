fc8d91b2a89d279b29b23f13fdf8e1710c5344d3
blackinitial <alhanif.blackinitial@gmail.com>
2023-06-21 04:06:17 +0700

    add prefetch, sitemap, astro compress, fix FCP

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import image from "@astrojs/image";
 import vercel from "@astrojs/vercel/serverless";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()],
+  site: "https://blint.dev",
+  integrations: [tailwind(), image(), react(), prefetch(), sitemap(), compress()],
   vite: {
     ssr: {
       noExternal: ["@radix-ui/*"]
